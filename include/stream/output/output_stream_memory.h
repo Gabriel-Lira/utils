3d6caf208ed2998439683ca50b1ca72d636644e1
gabriel.lira <ggabriel_al@hotmail.com>
2022-01-12 16:04:32 -0300

    # Added output stream memory.

---
+++ include/stream/output/output_stream_memory.h
@@ -0,0 +1,34 @@
+#ifndef OUTPUT_STREAM_MEMORY
+#define OUTPUT_STREAM_MEMORY
+
+#include "stream/output/output_stream_type.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// clang-format off
+#define os_memory_write(OS, VAR) os_type_write(OS.stream, VAR, os_memory_write_u8)
+// clang-format off
+
+typedef bool (*MemoryWriteCallback)(uint8_t *src_ptr, size_t size, size_t offset);
+
+typedef struct OutputStreamMemory
+{
+    OutputStreamType stream;
+    const size_t initial_offset;
+    const MemoryWriteCallback write_callback;
+} OutputStreamMemory;
+
+OutputStreamMemory os_memory_make(MemoryWriteCallback memory_write_callback, size_t memory_initial_offset, size_t size);
+
+void os_memory_write_u8(OutputStreamType *os, uint8_t src_var);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // OUTPUT_STREAM_MEMORY
---
+++ src/stream/output/output_stream_memory.c
@@ -0,0 +1,19 @@
+#include "stream/output/output_stream_memory.h"
+
+OutputStreamMemory os_memory_make(MemoryWriteCallback memory_write_callback,
+                                  size_t memory_initial_offset, size_t size)
+{
+    return (OutputStreamMemory){
+        .write_callback = memory_write_callback,
+        .initial_offset = memory_initial_offset,
+        .stream = (OutputStreamType){.size = size, .offset = 0}};
+}
+
+void os_memory_write_u8(OutputStreamType *os, uint8_t src_var)
+{
+    OutputStreamMemory *os_buffer = (OutputStreamMemory *)os;
+    const size_t write_offset =
+        (os_buffer->initial_offset + os_buffer->stream.offset++);
+
+    os_buffer->write_callback(&src_var, sizeof(uint8_t), write_offset);
+}
