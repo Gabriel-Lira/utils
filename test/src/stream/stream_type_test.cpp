b07c1126861bcb5ca745f60e46b7ac129af6ef93
gabriel.lira <ggabriel_al@hotmail.com>
2022-01-19 14:25:03 -0300

    # Changed stream type test file name, and fixed test, beyond to organize test cmake file.

--- test/src/stream/stream_type_test.cpp
+++ test/src/stream/stream_type_test.cpp
@@ -10,10 +10,9 @@ CATCH_REGISTER_TAG_ALIAS("[@stream_type]", TEST_DEFAULT_TAG)
 TEST_CASE("stream_type_make must creat StreamType correctly",
           TEST_DEFAULT_TAG "[stream_type_make]")
 {
-    // const size_t size = GENERATE(as<size_t>{}, 0, SIZE_MAX);
-    // const StreamType stream_expected = StreamType{.offset = 0, .size = size};
+    const size_t size = GENERATE(as<size_t>{}, 0, SIZE_MAX);
+    const StreamType stream_expected = StreamType{.offset = 0, .size = size};
+    const StreamType stream_made = stream_type_make(size);
 
-    // REQUIRE(stream_type_make(size) == stream_expected);
-
-    REQUIRE(true);
+    REQUIRE(stream_made == stream_expected);
 }
